f8eb014de9cd4e3d8a0f0e17c3461051e38da41c
Finn McCleary <mcclearyfinn@gmail.com>
2019-05-08 15:11:48 -0400

    Updated clipboardScript.java
    implemented the dictionary and put in some ways to test in the main method

--- clipboardScript.java
+++ clipboardScript.java
@@ -7,30 +7,52 @@ import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.IOException;
 public class clipboardScript{
 
+    //global dictionary declaration
+    static Dictionary<String, Integer> d = new Hashtable<String, Integer>();
 
     public static void main(String[] args){
-    		//prints out the clipboard, but goes really slow for some reason??
-  			System.out.println(readClipboard());
- 
+    	//prints out the clipboard, but goes really slow for some reason??
+  		System.out.println(readClipboard());
+        
+
+        //tests for the dictionary
+        addToDictionary("hi");
+        addToDictionary("hello");
+        addToDictionary("hi");
+        addToDictionary("hi");
+        //this prints the dictionary
+        Enumeration<Integer> i = d.elements();
+        for (Enumeration<String> k = d.keys(); k.hasMoreElements();) 
+        { 
+            System.out.println("Key: " + k.nextElement() + " Value: " + i.nextElement()); 
+        }  
     }
 
     public static String readClipboard(){
-    	String clipboard = "";
-    	try {
-  			clipboard = (String) Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor);
-		}
-    	catch(UnsupportedFlavorException e) {
-  			e.printStackTrace();
-		}
-		catch(IOException e) {
-  			e.printStackTrace();
-		}
-		return clipboard;
+      	String clipboard = "";
+      	try {
+      		clipboard = (String) Toolkit.getDefaultToolkit().getSystemClipboard().getData(DataFlavor.stringFlavor);
+  		}
+        catch(UnsupportedFlavorException e) {
+      		e.printStackTrace();
+  		}
+  		catch(IOException e) {
+      		e.printStackTrace();
+  		}
+  		return clipboard;
 	}
 
 
-	public static void addToDictionary(){
-
+	public static void addToDictionary(String s){
+        //check if already in dictionary
+        if(d.get(s)!=null){
+            //add 1 to counter
+            d.put(s, ((int) d.get(s)) + 1);
+        }
+        //otherwise put it into the dictionary
+        else{
+            d.put(s, 1);
+        }    
 	}
 
 
