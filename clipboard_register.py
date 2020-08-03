aee5f0e401915504593627233a55730ffad94984
kaustav-aarish <kaustav@aarishtech.com>
2020-08-03 13:16:59 -0400

    Update clipboard_register.py

--- clipboard_register.py
+++ clipboard_register.py
@@ -12,30 +12,6 @@ MAX_MEMORY = 3
 CURRENT_FRAME = 1
 CURRENT_CLIPBOARD_CONTENT = ''
 
-def checkClipboard():
-
-    TEMPORARY_CLIPBOARD_CONTENT = root.clipboard_get()
-
-    if CURRENT_CLIPBOARD_CONTENT != TEMPORARY_CLIPBOARD_CONTENT:
-        createFrame(TEMPORARY_CLIPBOARD_CONTENT)
-        CURRENT_CLIPBOARD_CONTENT = TEMPORARY_CLIPBOARD_CONTENT
-
-def createFrame(content):
-
-    global CURRENT_FRAME
-
-    t = time.localtime()
-    current_time = time.strftime("%H:%M:%S", t)
-    
-    tempFrame = tk.Frame(root, bg = '#f2f2f2', highlightbackground="black", highlightthickness=0.5)
-    tempFrame.place(relx=0,rely=(1/(MAX_MEMORY+1)) * (CURRENT_FRAME), relwidth=1 ,relheight = (1/(MAX_MEMORY+1)))
-    
-    message = "{}: {}".format(current_time, content)
-    label = tk.Label(tempFrame, textvariable=message)
-    label.place(relx=0.5,rely=0.5,relwidth=1, relheight = 1, anchor = 'n')
-    
-    CURRENT_FRAME += 1
-
 root = tk.Tk()
 
 canvas = tk.Canvas(root, height = CANVAS_HEIGHT, width = CANVAS_WIDTH)
@@ -48,8 +24,48 @@ backgroundImg = ImageTk.PhotoImage(Image.open("./assets/frame_background.jpg"))
 backroundLabel = tk.Label(titleFrame, image = backgroundImg)
 backroundLabel.place(relwidth=1, relheight=1)
 
-# lambda: checkClipboard()
-print(root.clipboard_get())
+TEMPORARY_CLIPBOARD_CONTENT = root.clipboard_get()
+
+if CURRENT_CLIPBOARD_CONTENT != TEMPORARY_CLIPBOARD_CONTENT:
+
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    
+    tempFrame = tk.Frame(root, bg = '#f2f2f2', highlightbackground="black", highlightthickness=0.5)
+    tempFrame.place(relx=0,rely=(1/(MAX_MEMORY+1)) * (CURRENT_FRAME), relwidth=1 ,relheight = (1/(MAX_MEMORY+1)))
+    message = "{}: {}".format(current_time, TEMPORARY_CLIPBOARD_CONTENT)
+    label = tk.Label(tempFrame, text=message)
+    label.place(relwidth=1, relheight = 1)
+    
+    CURRENT_FRAME += 1
+
+    CURRENT_CLIPBOARD_CONTENT = TEMPORARY_CLIPBOARD_CONTENT
+
+# while True: 
+
+#     TEMPORARY_CLIPBOARD_CONTENT = root.clipboard_get()
+
+#     if CURRENT_CLIPBOARD_CONTENT != TEMPORARY_CLIPBOARD_CONTENT:
+
+#         t = time.localtime()
+#         current_time = time.strftime("%H:%M:%S", t)
+        
+#         tempFrame = tk.Frame(root, bg = '#f2f2f2', highlightbackground="black", highlightthickness=0.5)
+#         tempFrame.place(relx=0,rely=(1/(MAX_MEMORY+1)) * (CURRENT_FRAME), relwidth=1 ,relheight = (1/(MAX_MEMORY+1)))
+#         message = "{}: {}".format(current_time, TEMPORARY_CLIPBOARD_CONTENT)
+#         label = tk.Label(tempFrame, text=message)
+#         label.place(relx=0.5,rely=0.5,relwidth=0.5, relheight = 0.5, anchor = 'n')
+        
+#         CURRENT_FRAME += 1
+
+#         CURRENT_CLIPBOARD_CONTENT = TEMPORARY_CLIPBOARD_CONTENT
+
+#     time.sleep(0.1)
+#     root.update_idletasks()
+#     root.update()
 
 root.mainloop()
-    
+
+# old 
+# new
+# newer
