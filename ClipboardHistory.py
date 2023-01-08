f4057cf7220669c48b26bea7a41087280fa608e7
Kaustav Das Sharma <kaustav.dassharma@mail.mcgill.ca>
2023-01-07 16:56:25 -0800

    Added scroll

--- ClipboardHistory.py
+++ ClipboardHistory.py
@@ -4,9 +4,17 @@ class ClipboardHistory:
     def __init__(self, filepath):
         self.filepath = filepath
         self.root = tk.Tk()
-        self.text = tk.Text(self.root)
-        self.text.pack()
-        self.update_text()
+
+        # Create a Scrollbar widget
+        scrollbar = tk.Scrollbar(self.root)
+        scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        # Create a Text widget and associate it with the Scrollbar
+        self.text = tk.Text(self.root, yscrollcommand=scrollbar.set)
+        self.text.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        scrollbar.config(command=self.text.yview)
+
+        self.update_text()  # Start updating the text
         self.root.mainloop()
 
     def update_text(self):
@@ -14,6 +22,12 @@ class ClipboardHistory:
             contents = f.read()
         self.text.delete(1.0, tk.END)  # Clear current text
         self.text.insert(tk.END, contents)  # Insert new text
+        
+        # Get the current scroll position
+        # current_scroll_position = self.text.yview()[0]
+        # self.text.see(current_scroll_position)  # Set the scroll position
+        self.text.see(tk.END)  # Scroll to the bottom
+
         self.root.after(1000, self.update_text)  # Schedule next update
 
 if __name__ == "__main__":
